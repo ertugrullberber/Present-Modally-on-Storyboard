00c71c9953d0f53c293902dfb818a0090c8a69b4
ertugrullberber <ertugrulberber3461@gmail.com>
2022-10-17 11:22:52 +0300

    first commit

---
+++ Present Modally App/ViewController2.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController2.swift
+//  Present Modally App
+//
+//  Created by Ertugrul Berber on 17.10.2022.
+//
+
+import UIKit
+
+class ViewController2: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+    @IBAction func tiklandiDismiss(_ sender: Any) {
+        
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+
+}
